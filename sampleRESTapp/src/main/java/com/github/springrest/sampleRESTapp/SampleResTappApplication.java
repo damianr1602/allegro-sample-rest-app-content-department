a0b64e12c293aaf5a6faadee2b472eb63feb79ea
Damian <damianr1602@gmail.com>
2018-04-30 08:28:47 +0200

    add spring initializr file

---
+++ sampleRESTapp/src/main/java/com/github/springrest/sampleRESTapp/SampleResTappApplication.java
@@ -0,0 +1,12 @@
+package com.github.springrest.sampleRESTapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleResTappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleResTappApplication.class, args);
+	}
+}
