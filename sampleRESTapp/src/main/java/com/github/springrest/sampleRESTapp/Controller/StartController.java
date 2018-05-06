5afa4fbd640067e2f854afe2e39ab9a6bb68412b
damian <damianr1602@gmail.com>
2018-05-06 20:11:54 +0200

    adding sample tests

--- sampleRESTapp/src/main/java/com/github/springrest/sampleRESTapp/Controller/StartController.java
+++ sampleRESTapp/src/main/java/com/github/springrest/sampleRESTapp/Controller/StartController.java
@@ -1,28 +1,17 @@
 package com.github.springrest.sampleRESTapp.Controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StartController implements ErrorController{
-
-    private static final String PATH = "/error";
+public class StartController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String start() {
         return "Internship rest app";
     }
 
-    @RequestMapping(value = PATH)
-    public String error() {
-        return "handling options: /films, films/film?id=nr, /add";
-    }
 
-    @Override
-    public String getErrorPath() {
-        return PATH;
-
-    }
 }
